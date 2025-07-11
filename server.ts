19e41ae1e0c728d9fa74df1e7f888d8af17ae9ae
Save1i <savva0512@gmail.com>
2025-07-11 18:11:51 +0300

    add: neon db and route

--- server.ts
+++ server.ts
@@ -2,9 +2,24 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+import route from "./src/routes/router"
+
+require("dotenv").config({ debug: true });
+import { neon } from "@neondatabase/serverless"
+const sql = neon(process.env.DATABASE_URL as string);
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+  try {
+    res.send(`Hello world!`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error querying database');
+  }
+});
+
+app.use("/", route)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
