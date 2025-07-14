a38ccd1c0b901a0fe95c8c2be6ef32e6b269e488
Save1i <savva0512@gmail.com>
2025-07-14 12:04:46 +0300

    add: CRUD operators

---
+++ src/controllers/categoryControllers.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import db from "../db/queries"
+
+async function createCategoryName(req: Request, res: Response) {
+    const {categoryName} = req.body
+    await db.insertCategoryName(categoryName)
+    res.status(200).json(categoryName)
+}
+
+async function updateCategoryName(req:Request, res: Response) {
+    const {categoryName, categoryId} = req.body;
+
+    await db.updateCategoryName(categoryName, categoryId)
+    res.status(200).json({categoryName, categoryId})
+}
+
+async function deleteCategory(req: Request, res:Response) {
+    const {categoryId} = req.body;
+
+    await db.deleteCategory(categoryId)
+    res.status(200).json(categoryId)
+}
+
+async function getAllCategories(req:Request, res: Response) {
+    const categories = await db.getAllCategories()
+    console.log(categories)
+    res.status(200).json(categories)
+}
+
+export = {
+    createCategoryName,
+    updateCategoryName,
+    deleteCategory,
+    getAllCategories,
+}
