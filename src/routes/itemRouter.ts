c761f629f51c29b43f7f60b021b1acff1c97d32c
Save1i <savva0512@gmail.com>
2025-07-14 17:55:47 +0300

    some changes

--- src/routes/itemRouter.ts
+++ src/routes/itemRouter.ts
@@ -3,11 +3,11 @@ import controller from "../controllers/itemControllers"
 
 const router = Router()
 
+router.get("/", controller.getAllItems)
 router.post("/new", controller.insertItem)
 router.get("/update", controller.getItem)
 router.post("/update", controller.updateItem)
 router.post("/delete", controller.deleteItem)
-router.get("/", controller.getAllItems)
 router.get("/:categoryId", controller.getCategoryItems)
 
 
