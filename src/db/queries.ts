2541f2b5555c23995239caf02d4533c4608c08bf
Save1i <savva0512@gmail.com>
2025-07-14 12:05:52 +0300

    add: queries

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -14,10 +14,37 @@ async function deleteCategory(categoryId:number) {
     await sql.query("DELETE FROM Categories WHERE category_id=($1)", [categoryId])
 }
 
+async function getAllCategories() {
+    const rows = await sql.query("SELECT * FROM Categories")
+    return rows
+}
+
+async function insertItem(itemName: string, category_id: number, item_count: number, item_price: number) {
+    await sql.query("INSERT INTO Items (item_name, category_id, item_count, item_price) VALUES ($1, $2, $3, $4)", [itemName, category_id, item_count, item_price]);
+}
+
+async function updateItem(itemName: string, itemId?:number, item_count?: number, item_price?: number, category_id?: number) {
+    await sql.query("UPDATE items SET item_name=$1, category_id=$5, item_count=$3, item_price=$4 WHERE item_id=$2", [itemName, itemId, item_count, item_price, category_id] )
+}
+
+async function deleteItem(itemId: number) {
+    await sql.query("DELETE FROM items WHERE item_id=($1)", [itemId])
+}
+
+async function getAllItems() {
+    const rows = await sql.query("SELECT * FROM items")
+    return rows
+}
+
 export = {
     insertCategoryName,
     updateCategoryName,
     deleteCategory,
+    getAllCategories,
+    insertItem,
+    updateItem, 
+    deleteItem,
+    getAllItems,
 }
 
     
