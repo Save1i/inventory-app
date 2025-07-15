3c5faf22dac65f4a828f58bcbf4cbbf437a061e7
Save1i <savva0512@gmail.com>
2025-07-15 09:30:41 +0300

    add: ejs

--- server.ts
+++ server.ts
@@ -1,14 +1,17 @@
 import express from 'express'
+import router from "./src/routes/index.ts"
+import { neon } from "@neondatabase/serverless"
+import path from 'path'
+
 const app = express()
+app.use(express.json());
+app.set('views', path.join(__dirname, 'src/views'))
+app.set('view engine', 'ejs')
+
 const port = 3000
 
-import router from "./src/routes/index.ts"
-
 require("dotenv").config({ debug: true });
-import { neon } from "@neondatabase/serverless"
 const sql = neon(process.env.DATABASE_URL as string);
-
-app.use(express.json());
 
 app.get('/', async (req, res) => {
   try {
