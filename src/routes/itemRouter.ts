5cf11c3173c80cbc19722de362e51779b421fb85
Save1i <savva0512@gmail.com>
2025-07-18 19:13:54 +0300

    add: get /new

--- src/routes/itemRouter.ts
+++ src/routes/itemRouter.ts
@@ -4,7 +4,8 @@ import controller from "../controllers/itemControllers"
 const router = Router()
 
 router.get("/", controller.getAllItems)
-router.post("/new", controller.insertItem)
+router.post("/new", controller.insertItemPost)
+router.get("/new", controller.insertItemGet)
 router.get("/:itemId/update", controller.getItemForUpdate)
 router.get("/:itemId", controller.getItem)
 router.post("/:itemId/update", controller.updateItem)
