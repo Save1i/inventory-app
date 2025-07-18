3ac3c6f630c1ad5e8ecbdde5f09565bade72398d
Save1i <savva0512@gmail.com>
2025-07-18 19:13:44 +0300

    add: get /new

--- src/routes/categoryRouter.ts
+++ src/routes/categoryRouter.ts
@@ -4,7 +4,8 @@ import controller from "../controllers/categoryControllers"
 const router = Router()
 
 router.get("/", controller.getAllCategories)
-router.post("/new", controller.createCategoryName)
+router.post("/new", controller.createCategoryNamePost)
+router.get("/new", controller.createCategoryNameGet)
 router.post("/update", controller.updateCategoryName)
 router.post("/delete", controller.deleteCategory)
 router.get("/:categoryId", controller.getCategoryItems)
