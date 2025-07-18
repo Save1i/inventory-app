e34b528cb7f4815989400d5143e266b4ff68c361
Save1i <savva0512@gmail.com>
2025-07-18 16:31:10 +0300

    add: getCategoryItems

--- src/routes/categoryRouter.ts
+++ src/routes/categoryRouter.ts
@@ -7,5 +7,6 @@ router.get("/", controller.getAllCategories)
 router.post("/new", controller.createCategoryName)
 router.post("/update", controller.updateCategoryName)
 router.post("/delete", controller.deleteCategory)
+router.get("/:categoryId", controller.getCategoryItems)
 
 export default router   
